1ddd0308133dc0a19f92b14d3701e2f4f6844d95
void <gautamdev095@gmail.com>
2026-09-15 14:21:30 +0530

    Start from scratch

--- src/constants/NodeTypes.js
+++
@@ -1,9 +0,0 @@
-export const NODE_TYPES = {
-   UNVISITED: "unvisited",
-   START: "start",
-   END: "end",
-   WALL: "wall",
-   VISITED: "visited",
-   SHORTEST_PATH: "shortest-path",
-   ACTIVE: "active",
-};
