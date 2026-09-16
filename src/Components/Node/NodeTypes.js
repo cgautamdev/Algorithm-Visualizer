6100489d36cb5d374c0d2ada77b0d64cf7b3d0dc
void <gautamdev095@gmail.com>
2026-09-16 17:12:18 +0530

    basic setup for the grid and nodes are done

--- src/Components/Node/NodeTypes.js
+++ src/Components/Node/NodeTypes.js
@@ -2,6 +2,11 @@ const NodeTypes = {
    START_NODE: "start-node",
    END_NODE: "end-node",
    UNVISITED: "unvisited",
+   VISITED: "visited",
+   ACTIVE: "active",
+   WALL: "wall",
+   WEIGHT: "weight",
+   PATH: "path",
 };
 
 export default NodeTypes;
